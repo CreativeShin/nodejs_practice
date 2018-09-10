f9ef652712df61f7f1d3c67ba3257bf10c904374
Creative Shin <creative-shin@outlook.com>
2018-09-10 16:48:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append the file to the log!');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next)=>{
+    res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine','hbs');
+
+app.get('/', (req, res)=>{
+    res.render('home', {
+        pageTitle: "Home Page!",
+        welcomeMessage: 'Welcome to home!'
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about',{
+        pageTitle: 'about page'
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Server is running on the port 3000');
+}); 
